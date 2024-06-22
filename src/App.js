810e9a52b7d06a870d902a960bdddba9778fd734
budimbu esuku <budimbuesu@gmail.com>
2024-06-22 22:52:47 +0200

    local storage added. Lorsque l on recharge la page, on le perd pas les tâches ajoutées

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./component/TodoInput";
 import TodoList from "./component/TodoList";
 import "./index.css";
@@ -7,8 +7,12 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
 
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
   const handleTodoList = (newTodo) => {
     const tmp = [...todos, newTodo];
+    persistData(tmp);
     setTodos(tmp);
   };
 
@@ -16,6 +20,7 @@ function App() {
     const todosAfterDeleted = todos.filter((aTodo, index) => {
       return index !== indexOfTodoToDelete;
     });
+    persistData(todosAfterDeleted);
     setTodos(todosAfterDeleted);
   }
 
@@ -24,6 +29,20 @@ function App() {
     setNewTodo(valueToBeEdited);
     handleDeleteTodo(index);
   }
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem("todos");
+
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
+
   return (
     <>
       <TodoInput
